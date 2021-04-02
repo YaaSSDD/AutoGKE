27e880d35bed28a16f50dd8847ab16177a9c0370
Yassine M'HAIA <yassinemhaia@gmail.com>
2021-04-02 03:21:54 +0200

    add function

--- AutoGKE.sh
+++ AutoGKE.sh
@@ -1,106 +1,101 @@
 #!/bin/sh
 
-read -p "entrée l'ID de votre projet" IDPROJECT
+read -p " entrée l'ID de votre projet : " IDPROJECT
 export PROJECT_ID=$IDPROJECT
-read -p "entrée le nom de votre cluster : " CLUSTERNAME
-echo $CLUSTERNAME
-read -p "entrée le port use par le load-balancer : " LBPORT
-echo $LBPORT
-read -p "entrée le nombre de nodes dans votre cluster : " NBNODE
-echo $NBNODE
-read -p "entrée le nom de notre deployment : " DEPLOYNAME
-echo $DEPLOYNAME
 
+GKE ()
+{
+    
+gcloud compute zones list
+read -p "set-up your zone : " ZONE
+
+read -p " entrée le nom de votre cluster : " CLUSTERNAME
+echo $CLUSTERNAME
+read -p " entrée le port use par le load-balancer : " LBPORT
+echo $LBPORT
+read -p " entrée le nombre de nodes dans votre cluster : " NBNODES
+echo $NBNODES
+read -p " entrée le nom de notre deployment : " DEPLOYNAME
+echo $DEPLOYNAME
+read -p " entrée le nombre de minimum (NODES) : " MINNODES
+echo $MINNODES
+read -p " entrée le nombre de scalling maximum (NODES) : " MAXNODES
+echo $MAXNODES
 gcloud config set project $PROJECT_ID
 
-gcloud config set compute/zone europe-west1-b
+gcloud config set compute/zone $ZONE
 
 
-gcloud container clusters create $CLUSTERNAME --num-nodes=$NBNODE \
- --enable-autoscaling \
- --min-nodes=1 \
- --max-nodes=10
+gcloud container clusters create $CLUSTERNAME \
+  --num-nodes=$NBNODES \
+  --enable-autoscaling \
+  --min-nodes=$MINNODES \
+  --max-nodes=$MAXNODES
 
 
 gcloud compute instances list
 
+}
 
-read -p "entrée le nombre de projet à  containeuriser" CONTAINERNB
+
+GCR()
+{
+read -p "entrée le nombre de projet à  containeuriser : " CONTAINERNB
+
 i=1
 
-
 ###boucle build tag->push->images on GoogleContainerRegistry
-for x in `seq 1 $CONTAINERNB`;
-do
-        echo $i
+  for x in `seq 1 $CONTAINERNB`;
+  do
+    echo $i
+    read -p "nom de votre container $i : " CONTAINER
 
-        read -p "nom de votre container $i ?" CONTAINER
+    i=$(($i + 1))
+    echo "$CONTAINER${i}"
 
-        i=$(($i + 1))
-        echo "$CONTAINER${i}"
+    read -p "entrée le nommage de votre image : " IMAGENAME
+    echo $IMAGENAME
 
+    read -p "entrée votre tag : " TAG
+    echo $TAG
 
-read -p "entrée le nommage de votre image : " IMAGENAME
-echo $IMAGENAME
+    read -p "entrée le port use par le pod : " PODPORT
+    echo $PODPORT
 
+    read -p "entrée le repertoire contenant votre app/Dockerfile : " DIRPROJECT
+    echo $DIRPROJECT
 
-read -p "entrée votre tag : " TAG
-echo $TAG
+    echo "gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG ./$DIRPROJECT "
 
+    docker build -t gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG ./$DIRPROJECT
 
-
-
-read -p "entrée le port use par le pod : " PODPORT
-echo $PODPORT
-
-
-read -p "entrée le repertoire contenant votre app/Dockerfile : " DIRPROJECT
-echo $DIRPROJECT
-
-
-
-echo "gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG ./$DIRPROJECT"
-
-
-docker build -t gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG ./$DIRPROJECT
-
-gcloud auth configure-docker
-
-
-
-docker push gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG
-
-kubectl create deployment $DEPLOYNAME --image=gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG
-
-kubectl get pods
-
+    gcloud auth configure-docker
+    
+    docker push gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG
+    kubectl create deployment $DEPLOYNAME --image=gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG
+    kubectl get pods
      
 
-done
-
+  done
+}
    
 
 
-
+EXPOSE ()
+{
 ##test pull from GCR 
 #docker run --rm -p $PODPORT:$PODPORT gcr.io/${PROJECT_ID}/$IMAGENAME:$TAG
 
 #curl http://localhost:$PODPORT
 
-read -p "entrée le nom de votre deployment à exposé" CONTAINERPHARE
+  read -p "entrée le nom de votre deployment à exposé : " CONTAINERPHARE
 
 
 
 #L'indicateur --port spécifie le numéro de port configuré sur l'équilibreur de charge 
 #L'indicateur --target-port indique le numéro de port utilisé par le pod 
-kubectl expose deployment $CONTAINERPHARE --type=LoadBalancer --port $LBPORT --target-port $PODPORT
-
-
-
-
-
-
-
-
-
-
+  kubectl expose deployment $CONTAINERPHARE --type=LoadBalancer --port $LBPORT --target-port $PODPORT
+}
+GKE
+GCR
+#EXPOSE
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #Login Project in Gcloud CLI
 **Automatic deploy stack Google Kubernetes Engine &amp;&amp; Build\Tag\Push Regitry Google Container Registry**
-
+#Place your directory containing your docker images at the root(example DD directory) of the project after -> 
 ```
 sudo chmod 755 AutoGKE.sh
 ./AutoGKE.sh
@@ -11,4 +11,6 @@ sudo chmod 755 AutoGKE.sh
 #Follow the instructions of the terminal
 
 #The Script deploy stack GoogleKubernesEngine with Google Container Registry
+
+
 
